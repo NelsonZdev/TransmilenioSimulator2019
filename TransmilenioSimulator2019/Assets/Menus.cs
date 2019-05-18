54bb292d55e7a09009aac8ce9ddf8ce04443070a
David Bricestone <29939205+jdbu2002@users.noreply.github.com>
2019-05-18 00:53:41 -0500

    Menu Mantenimiento

--- TransmilenioSimulator2019/Assets/Menus.cs
+++ TransmilenioSimulator2019/Assets/Menus.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class Menus : MonoBehaviour
 {
-    public Animator menu;
-    bool opened = false;
+    public Animator menu, mant;
+    bool openedM = false, openedN = false;
     
-    public void Presion(){
-        opened = !opened;
+    public void Presion1(){
+        openedM = !openedM;
 
-        if(opened){
+        if(openedM){
             menu.SetBool("Panel_Compra_Open", true);
+            mant.SetBool("Panel_Mant_Open", false);
         }
         else{
             menu.SetBool("Panel_Compra_Open", false);
         }
 
     }
+    
+    public void Presion2(){
+        openedN = !openedN;
+
+        if(openedN){
+            mant.SetBool("Panel_Mant_Open", true);
+            menu.SetBool("Panel_Compra_Open", false);
+        }
+        else{
+            mant.SetBool("Panel_Mant_Open", false);
+        }
+
+    }
 
     public void allOut(){
         menu.SetBool("Panel_Compra_Open", false);
+        mant.SetBool("Panel_Mant_Open", false);
     }
 
 }
