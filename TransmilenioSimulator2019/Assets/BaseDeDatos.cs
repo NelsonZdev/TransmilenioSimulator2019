1bb21864964b8c8dec1a57254e1c603e10b6f1e7
Nelson Zapata <46973364+NelsonZdev@users.noreply.github.com>
2019-05-17 20:40:46 -0500

    Base Json
    Agregado un nuevo Json

--- TransmilenioSimulator2019/Assets/BaseDeDatos.cs
+++ TransmilenioSimulator2019/Assets/BaseDeDatos.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class BaseDeDatos : MonoBehaviour
 {
-    public float dato;
+    [HideInInspector]
+    public Datos claseDatos;
+    private float dato;
+    private string ubicacionGuardado;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        ubicacionGuardado = Application.dataPath;
+    }
     void Start()
     {
-        
+        claseDatos = new Datos()
+        {
+            info1 = "NuevoJson2",
+            dato_principal = 53
+        };
+        string archivoJson = JsonUtility.ToJson(claseDatos);
+        print(archivoJson);
+
+        File.WriteAllText(ubicacionGuardado + "Base_de_Datos", archivoJson);
+        print(ubicacionGuardado);
+
+        Datos cargarDatos = JsonUtility.FromJson<Datos>(archivoJson);
+        print("float: "+cargarDatos.dato_principal);
+        print("String: " + cargarDatos.info1);
     }
 
     // Update is called once per frame
@@ -16,4 +37,10 @@ public class BaseDeDatos : MonoBehaviour
     {
         
     }
+}
+[System.Serializable]
+public class Datos
+{
+    public string info1;
+    public float dato_principal;
 }
