55067b6037c1bd2a732347d2de1a7d9d20638653
David Bricestone <29939205+jdbu2002@users.noreply.github.com>
2019-05-18 06:32:34 -0500

    aSSETS

---
+++ TransmilenioSimulator2019/Assets/Map/Tiles.tsx
@@ -0,0 +1,175 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Tiles" tilewidth="128" tileheight="128" tilecount="57" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_49.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_50.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_51.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_52.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_53.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_54.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_55.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_56.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_57.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_01.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_02.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_03.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_04.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_05.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_06.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_07.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_08.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_09.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_10.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_11.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_12.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_13.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_14.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_15.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_16.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_17.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_18.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_19.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_20.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_21.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_22.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_23.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_24.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_25.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_26.png"/>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_27.png"/>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_28.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_29.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_30.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_31.png"/>
+ </tile>
+ <tile id="40">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_32.png"/>
+ </tile>
+ <tile id="41">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_33.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_34.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_35.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_36.png"/>
+ </tile>
+ <tile id="45">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_37.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_38.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_39.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_40.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_41.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_42.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_43.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_44.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_45.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_46.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_47.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="128" source="../../Pixel_2D_Isometric_Road_Devils_WorkShop_v01/Pixel_2D_Isometric_Road_Devils_WorkShop_v01/road_isometric/road_48.png"/>
+ </tile>
+</tileset>
